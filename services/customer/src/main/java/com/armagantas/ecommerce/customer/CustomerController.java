e85f24454679d3ef753fe9fddc6e07029528fa7a
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-06-20 02:55:53 +0300

    Customer microservice implementation is done.

--- services/customer/src/main/java/com/armagantas/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/armagantas/ecommerce/customer/CustomerController.java
@@ -2,11 +2,11 @@ package com.armagantas.ecommerce.customer;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -19,5 +19,40 @@ public class CustomerController {
             @RequestBody @Valid CustomerRequest request
     ) {
        return ResponseEntity.ok(_customerService.createCustomer(request));
+    }
+    
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        _customerService.updateCustomer(request);
+        return ResponseEntity.accepted().build();
+    }  
+    
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> getAllCustomers() {
+        return ResponseEntity.ok(_customerService.findAllCustomers());
+    }
+    
+    @GetMapping("/exists/{customer-id}")
+    public ResponseEntity<Boolean> existsById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        return ResponseEntity.ok(_customerService.existsById(customerId));
+    }
+    
+    @GetMapping("/{customer-id}")
+    public ResponseEntity<CustomerResponse> getCustomerById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        return ResponseEntity.ok(_customerService.findById(customerId));
+    }
+    
+    @DeleteMapping("/{customer-id}")
+    public ResponseEntity<Void> deleteCustomerById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        _customerService.deleteCustomer(customerId);
+        return ResponseEntity.accepted().build();
     }
 }
--- services/customer/src/main/java/com/armagantas/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/armagantas/ecommerce/customer/CustomerService.java
@@ -1,18 +1,74 @@
 package com.armagantas.ecommerce.customer;
 
+import com.armagantas.ecommerce.exception.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.mapper.Mapper;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
-    
+
     private final CustomerRepository _customerRepository;
-    
+
     private final CustomerMapper _customerMapper;
-    
+
     public String createCustomer(CustomerRequest request) {
         var customer = _customerRepository.save(_customerMapper.toCustomer(request));
         return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = _customerRepository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("Customer with id %s not found", request.id())
+                ));
+        mergerCustomer(customer, request);
+        _customerRepository.save(customer);
+    }
+
+    private void mergerCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstName())) {
+            customer.setFirstName(request.firstName());
+        }
+
+        if (StringUtils.isNotBlank(request.lastName())) {
+            customer.setLastName(request.lastName());
+        }
+
+        if (StringUtils.isNotBlank(request.email())) {
+            customer.setEmail(request.email());
+        }
+
+        if (request.address() != null) {
+            customer.setAddress(request.address());
+        }
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return _customerRepository.findAll()
+                .stream()
+                .map(_customerMapper :: fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public Boolean existsById(String customerId) {
+        return _customerRepository.findById(customerId).isPresent();
+    }
+
+    public CustomerResponse findById(String customerId) {
+        return _customerRepository.findById(customerId)
+                .map(_customerMapper ::fromCustomer)
+                .orElseThrow(() -> new CustomerNotFoundException(format("Customer with id %s not found", customerId)));
+    }
+
+    public void deleteCustomer(String customerId) {
+        _customerRepository.deleteById(customerId);
     }
 }
