964ff75c0c1cd7d41818411d71bece440fab28fe
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-06-28 03:15:54 +0300

    Payment service and kafka integration done.

---
+++ services/payment/src/main/java/com/armagantas/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,20 @@
+package com.armagantas.ecommerce.notification;
+
+import com.armagantas.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        
+        BigDecimal amount,
+        
+        PaymentMethod paymentMethod,
+        
+        String customerFirstName,
+        
+        String customerLastName,
+        
+        String customerEmail
+) {
+}
--- services/payment/src/main/java/com/armagantas/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/armagantas/ecommerce/payment/PaymentService.java
@@ -1,5 +1,7 @@
 package com.armagantas.ecommerce.payment;
 
+import com.armagantas.ecommerce.notification.NotificationProducer;
+import com.armagantas.ecommerce.notification.PaymentNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,21 @@ public class PaymentService {
     
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+    private final NotificationProducer notificationProducer;
     
     public Integer createPayment(PaymentRequest request) {
         var payment = paymentRepository.save(paymentMapper.toPayment(request));
+        
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
         return payment.getId();
     }
 }
