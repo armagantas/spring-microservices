887555342278e813d6a9f8fe8b9f0bf9e0da7d26
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-07-02 01:04:23 +0300

    notification service done.

--- services/notification/src/main/java/com/armagantas/ecommerce/email/EmailService.java
+++ services/notification/src/main/java/com/armagantas/ecommerce/email/EmailService.java
@@ -1,5 +1,6 @@
 package com.armagantas.ecommerce.email;
 
+import com.armagantas.ecommerce.kafka.order.Product;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +17,17 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static com.armagantas.ecommerce.email.EmailTemplates.ORDER_CONFIRMATION;
 import static com.armagantas.ecommerce.email.EmailTemplates.PAYMENT_CONFIRMATION;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class EmailService {
-    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
     
@@ -51,6 +53,42 @@ public class EmailService {
         context.setVariables(model);
         messageHelper.setSubject(PAYMENT_CONFIRMATION.getTemplate());
         
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            messageHelper.setText(htmlTemplate, true);
+            messageHelper.setTo(destinationEmail);
+            mailSender.send(mimeMessage);
+            log.info("Email sent successfully");
+        } catch (MessagingException e) {
+            log.warn("Warning- Cannot send email to {}", destinationEmail);
+        }
+    }
+
+    @Async
+    public void sendOrderSuccessEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference,
+            List<Product> products
+    ) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper =
+                new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, UTF_8.name());
+        messageHelper.setFrom("armagantas6@gmail.com");
+        final String templateName = ORDER_CONFIRMATION.getTemplate();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("customerName", customerName);
+        model.put("totalAmount", amount);
+        model.put("orderReference", orderReference);
+        model.put("products", products);
+
+
+        Context context = new Context();
+        context.setVariables(model);
+        messageHelper.setSubject(ORDER_CONFIRMATION.getTemplate());
+
         try {
             String htmlTemplate = templateEngine.process(templateName, context);
             messageHelper.setText(htmlTemplate, true);
